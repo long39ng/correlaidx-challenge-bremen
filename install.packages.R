9f72f84a8c76959078e5eccebbd3917cc0699bb6
Frie <fr1e@pm.me>
2020-10-13 10:02:33 +0200

    higher versions of highcharter and dplyr

--- install.packages.R
+++ install.packages.R
@@ -1,7 +1,9 @@
-packages <- c("remotes", "rmarkdown", "flexdashboard", "igraph", "reactable", "htmltools", "highcharter", "shiny", "dplyr")
+packages <- c("remotes", "rmarkdown", "flexdashboard", "igraph", "reactable", "htmltools")
 
 for (p in packages) {
    install.packages(p)
 }
 
 remotes::install_github("hadley/emo")
+remotes::install_version("dplyr", "1.0.2", repos = "cran.rstudio.com")
+remotes::install_version("highcharter", "0.8.2", repos = "cran.rstudio.com")
