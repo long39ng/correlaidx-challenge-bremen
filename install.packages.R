c4f959e499a0a69bc78bf76f5826347de6cdbd6d
Frie <fr1e@pm.me>
2020-10-13 12:53:00 +0200

    explanation for fixing  dplyr / highcharter

--- install.packages.R
+++ install.packages.R
@@ -5,5 +5,8 @@ for (p in packages) {
 }
 
 remotes::install_github("hadley/emo")
+# fixing dplyr and highcharter versions is necessary
+# bc the default install.packages are older versions that result in a nasty error
+# related to `hc-key` inside mutate (did not find an issue but it definitely didn't work)
 remotes::install_version("dplyr", "1.0.2", repos = "cran.rstudio.com")
 remotes::install_version("highcharter", "0.8.2", repos = "cran.rstudio.com")
